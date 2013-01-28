a01e5b2ee77ddb93a2aaca68f6284e071a16e3f5
steveoh <sgourley@utah.gov>
2013-01-28 14:03:18 -0700

    requirements so far
    setup.py isn't quite making sense to me at this point but i have a
    boilerplate in there. pip will install the stuff from the requirements
    text file.

---
+++ setup.py
@@ -0,0 +1,23 @@
+##import agrc namespace
+#
+#try:
+#    from setuptools import setup
+#except ImportError:
+#    from distutils.core import setup
+#
+#packages = []
+#
+#requires = []
+#
+#setup(
+#    name='automatedTileCaching',
+#    version= 0.0.1 #namespace.__version__,
+#    description='AGRC Automated Tile Cacing',
+#    author='AGRC',
+#    author_email='agrc@utah.gov',
+#    url='http://gis.utah.gov',
+#    packages=packages,
+#    install_requires=requires
+#)
+
+pip install -qr requirements.txt
