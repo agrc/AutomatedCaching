39c3a1d85b30946c620b6afdb2cd04393644ed9e
steveoh <sgourley@utah.gov>
2013-01-29 16:50:17 -0700

    storing ags connection secrets

--- agrc/caching/commands/connect.py
+++ agrc/caching/commands/connect.py
@@ -1,17 +1,23 @@
 from agrc.caching.abstraction.base import Command
 from agrc.caching.config import Server
 import requests
+import ConfigParser
+import os
 
 class GetTokenCommand(Command):
+    section = "ArcGIS Admin Credentials"
+    
     def execute(self):
         server = Server()
         server.usePort = True
         
         tokenUrl = server.getTokenUrl()
         
+        credentials = self._getArcGisCredentials()
+          
         payload = {
-                   'username': '', 
-                   'password': '',
+                   'username': credentials[0], 
+                   'password': credentials[1],
                    'f': 'json'
         }
         
@@ -25,3 +31,17 @@ class GetTokenCommand(Command):
         response = r.json()
         
         return response['token']
+    
+    def _getArcGisCredentials(self):
+        config = ConfigParser.RawConfigParser()
+        
+        file = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", 'secrets.cfg')
+        
+        print file
+        
+        config.read(file)
+        
+        user = config.get(self.section, "username")
+        password = config.get(self.section, "password")
+        
+        return user, password
