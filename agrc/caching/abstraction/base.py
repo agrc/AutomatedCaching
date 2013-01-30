4eed3b340012b67142a77824e4ce6813e2e706de
steveoh <sgourley@utah.gov>
2013-01-30 13:57:14 -0700

    partial fill

--- agrc/caching/abstraction/base.py
+++ agrc/caching/abstraction/base.py
@@ -1,3 +1,15 @@
 class Command(object):
     def execute(self):
         raise NotImplementedError( "Should have implemented this" )
+    
+class CachingProcess(object):
+    socProcesses = "#"
+    servicePath = "" #
+    serviceName = "" #
+    scales = [] #
+    scaleService = ""
+    areaOfInterest = "" # trumps updateExtent
+#    updateMode = "RECREATE_ALL_TILES"
+    updateMode = "RECREATE_EMPTY_TILES"
+    gdbPath = "C:\\Cache\\MapData\\UtahBaseMap-Data.gdb\\"
+    gp = ""
