8a420673714aee2be5a255b95ee037ab07be3eec
steveoh <sgourley@utah.gov>
2013-01-31 21:40:45 -0700

    some inline documentation

--- agrc/caching/commands/cache.py
+++ agrc/caching/commands/cache.py
@@ -3,16 +3,22 @@ from agrc.caching.commands import connect
 from agrc.caching.config import Server
 
 class CacheStatusCommand(Command):
-    _server = None
+    """
+        Returns true if the number of busy instances on the server
+        are greater than 0. This tells us the server is caching a job already.
+    """
+    
+    #: the arcgis server configuration
+    server = None
     
     def __init__(self, server = Server()):
-        self._server = server
+        self.server = server
         
     def execute(self):
-        command = connect.GetTokenCommand(server = self._server)
+        command = connect.GetTokenCommand(server = self.server)
         token = command.execute()
         
-        command = connect.GetServiceStatisticsCommand("CachingTools.GPServer", token, self._server)
+        command = connect.GetServiceStatisticsCommand("CachingTools.GPServer", token, self.server)
         stats = command.execute()
         
         busy = stats['summary']['busy']
@@ -23,15 +29,49 @@ class CacheStatusCommand(Command):
         return False
     
 class ProcessChangeGeometryCommand(Command):
-    _changes = None
-    _arcpy = None
+    """
+        Command to possibly normalize and dissolve geometries to be cached
+        This class may be unused not sure yet.
+    """
+    
+    #: area of change 
+    changes = None
+    
+    #: arcgis python module
+    arcpy = None
     
     def __init__(self, changes, arcpy):
-        self._changes = changes
-        self._arcpy = arcpy
+        self.changes = changes
+        self.arcpy = arcpy
     
     def execute(self):
-        self._merge_geometries(changes)
+        pass
         
     def _merge_geometries(self, changes):
         pass
+    
+    def _intersect_geometry_to_scale_extent(self, changes):
+        pass
+    
+    def _dissolve_geometries(self, changes):
+        pass
+
+class ProccessJobCommand(Command):
+    """
+        A command for kicking off a cache
+    """
+    
+    #: The current job to cache
+    job = None
+    
+    #: arcgis python module
+    arcpy = None
+    
+    def __init__(self, job, arcpy):
+        self.job = job
+        self.arcpy = arcpy
+        
+    def execute(self):
+        pass
+    
+    
