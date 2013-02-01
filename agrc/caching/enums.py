520325871994deba52513c5f6cdf9310cce47d9d
steveoh <sgourley@utah.gov>
2013-01-31 23:19:31 -0700

    basemap cache names enum

--- agrc/caching/enums.py
+++ agrc/caching/enums.py
@@ -6,4 +6,14 @@ def enum(**enums):
     return type('Enum', (), enums)
 
 class CacheUpdateModes(object):
-    modes = enum(ALL = "RECREATE_ALL_TILES", EMPTY = "RECREATE_EMPTY_TILES")
+    modes = enum(ALL = "RECREATE_ALL_TILES", 
+                 EMPTY = "RECREATE_EMPTY_TILES")
+    
+class BaseMaps(object):
+    name = enum(TERRAIN = "terrain", 
+                VECTOR = "vector",
+                LITE = "lite",
+                IMAGERY = "imagery",
+                TOPO = "topo",
+                HILLSHADE = "hillshade",
+                HYBRID = "hybrid")
