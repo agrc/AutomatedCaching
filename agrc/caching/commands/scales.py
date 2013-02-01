2ad67e14d26109ff8befb6c284d2476006041217
steveoh <sgourley@utah.gov>
2013-01-31 22:07:50 -0700

    introducing the get scale from level class

--- agrc/caching/commands/scales.py
+++ agrc/caching/commands/scales.py
@@ -42,3 +42,45 @@ class GetCacheJobFromAreaOfChangeCommand(Command):
 
     def _intersect(self, a, b):
         return list(set(a) & set(b))
+
+class GetUtmScaleFromLevelCommand(Command):
+    """
+        Returns utm scale from scale level number in the format
+        that the geoprocessing tool likes.
+        
+        `[14] => '1128.497176'`
+        `[13, 14] => '2256.994353;1128.497176'`
+        
+        :keyword level_scale_map: see :attr:`level_scale_map`
+        :keyword levels: see :attr:`levels`
+    """
+    
+    #: the mapping betten the cache level and the utm scale
+    level_scale_map = { 
+        0: '18489297.737236',
+        1: '9244648.868618',
+        2: '4622324.434309',
+        3: '2311162.217155',
+        4: '1155581.108577',
+        5: '577790.554289',
+        6: '288895.277144',
+        7: '144447.638572',
+        8: '72223.819286',
+        9: '36111.909643',
+        10: '18055.954822',
+        11: '9027.977411',
+        12: '4513.988705',
+        13: '2256.994353',
+        14: '1128.497176'
+    }
+    
+    #: the cache level as an array
+    levels = None
+    
+    def __init__(self, levels):
+        self.levels = levels
+        
+    def execute(self):
+        scales = [key for level, key in levelScaleMap.iteritems() if level in levels]
+        
+        return ';'.join(scales)
