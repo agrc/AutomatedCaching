e990f4ba519f117e00336210b66835108a229da9
steve <sgourley@utah.gov>
2013-02-06 16:43:29 -0700

    more tests

--- tests/testSde.py
+++ tests/testSde.py
@@ -9,7 +9,7 @@ class TestConnect(TestCase):
     @patch.object(Geodatabase,'change_feature_class')
     @patch.object(Geodatabase,'path')
     def test_command_can_connect_to_file_gdb(self, path_mock, fc_mock):
-        print "test_command_can_connect_to_file_gdb"
+        print "test_can_connect_to_file_gdb"
         
         path_mock.__get__ = Mock(return_value="\Test_AreaOfChange.gdb")
         fc_mock.__get__ = Mock(return_value="AreaOfChange_Sorting")
@@ -19,3 +19,16 @@ class TestConnect(TestCase):
         
         self.assertIsNotNone(result, "query is broken")
         self.assertEqual(len(result), 3, "no areas of change")
+        
+    @patch.object(Geodatabase,'change_feature_class')
+    @patch.object(Geodatabase,'path')
+    def test_can_sort(self, path_mock, fc_mock):
+        print "test_command_can_connect_to_file_gdb"
+        
+        path_mock.__get__ = Mock(return_value="\Test_AreaOfChange.gdb")
+        fc_mock.__get__ = Mock(return_value="AreaOfChange_Sorting")
+
+        query = sde.AreasOfChangeQuery()
+        result = query.execute()
+        
+        self.assertLess(result[0].creation_date, result[1].creation_date, "order is wrong")
