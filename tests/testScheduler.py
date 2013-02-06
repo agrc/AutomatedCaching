648624c4fc0215b7ecb26026131a7effbd8e503c
steve <sgourley@utah.gov>
2013-02-06 16:50:34 -0700

    don't be lazy

--- tests/testScheduler.py
+++ tests/testScheduler.py
@@ -1,35 +1,35 @@
 from agrc.caching.scheduled import Runner
 from agrc.caching.models import AreaOfChange
 from mock import patch
+from nose.tools import nottest
+from unittest import TestCase
 
-@patch('agrc.caching.scheduled.sde.AreasOfChangeQuery')
-@patch('agrc.caching.scheduled.cache.CacheStatusCommand')
-def testAreasOfChangeNotQueriedIfCurrentlyCaching(cache_mock, query_mock):
-    cache_instance = cache_mock.return_value
-    cache_instance.execute.return_value = True
+#@nottest
+class TestScheduleRunner(TestCase):
     
-    s = Runner()
-    s.start()
+    @patch('agrc.caching.scheduled.sde.AreasOfChangeQuery')
+    @patch('agrc.caching.scheduled.cache.CacheStatusCommand')
+    def test_areas_of_change_not_queried_if_caching(self, cache_mock, query_mock):
+        cache_instance = cache_mock.return_value
+        cache_instance.execute.return_value = True
         
-    print cache_mock.call_count
-    print query_mock.call_count
- 
-    print dir(cache_instance)
-    
-    assert cache_mock.called
-    assert not query_mock.called
-
-@patch('agrc.caching.scheduled.sde.AreasOfChangeQuery')
-@patch('agrc.caching.scheduled.cache.CacheStatusCommand')  
-def testAreasOfChangeQueriedIfNotCaching(cache_mock, query_mock):
-    cache_instance = cache_mock.return_value
-    cache_instance.execute.return_value = False
-    
-    query_instance = query_mock.return_value
-    query_instance.execute.return_value = [AreaOfChange() for _ in xrange(2)]
-    
-    s = Runner()
-    s.start()
+        s = Runner()
+        s.start()
         
-    assert cache_mock.called
-    assert query_mock.called
+        assert cache_mock.called
+        assert not query_mock.called
+    
+    @patch('agrc.caching.scheduled.sde.AreasOfChangeQuery')
+    @patch('agrc.caching.scheduled.cache.CacheStatusCommand')  
+    def test_areas_of_change_queried_if_not_caching(self, cache_mock, query_mock):
+        cache_instance = cache_mock.return_value
+        cache_instance.execute.return_value = False
+        
+        query_instance = query_mock.return_value
+        query_instance.execute.return_value = [AreaOfChange() for _ in xrange(2)]
+        
+        s = Runner()
+        s.start()
+            
+        assert cache_mock.called
+        assert query_mock.called
