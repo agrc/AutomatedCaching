3de3fbbdaa957a82eacbf62fac006417ed176657
steve <sgourley@utah.gov>
2013-02-06 11:02:43 -0700

    remove weird class

--- agrc/caching/abstraction/base.py
+++ agrc/caching/abstraction/base.py
@@ -1,15 +1,3 @@
 class Command(object):
     def execute(self):
         raise NotImplementedError( "Should have implemented this" )
-    
-class CachingProcess(object):
-    socProcesses = "#"
-    servicePath = None #
-    serviceName = None #
-    scales = [] #
-    scaleService = None
-    areaOfInterest = None# trumps updateExtent
-#    updateMode = "RECREATE_ALL_TILES"
-    updateMode = "RECREATE_EMPTY_TILES"
-    gdbPath = "C:\\Cache\\MapData\\UtahBaseMap-Data.gdb\\"
-    gp = None
