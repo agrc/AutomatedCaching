869b3b448ad9752790d07fd393f6cbe741559e99
steve <sgourley@utah.gov>
2013-02-06 11:03:40 -0700

    refactored test

--- tests/testScheduler.py
+++ tests/testScheduler.py
@@ -1,33 +1,36 @@
 from agrc.caching.scheduled import Runner
+from agrc.caching import config
 from agrc.caching.models import AreaOfChange
-from mock import Mock, patch
+from mock import patch
 
-def testAreasOfChangeNotQueriedIfCurrentlyCaching():    
-    with patch('agrc.caching.scheduled.cache.CacheStatusCommand') as cache_mock:
-        with patch('agrc.caching.scheduled.sde.AreasOfChangeQuery') as query_mock:
-            cache_instance = cache_mock.return_value
-            cache_instance.execute.return_value = True
-            
-            query_instance = query_mock.return_value
-            query_instance.execute.return_value = []
-            
-            s = Runner()
-            s.start()
-        
-    cache_mock.assert_called_once_with()
-    assert not query_mock.execute.called
+@patch('agrc.caching.scheduled.sde.AreasOfChangeQuery')
+@patch('agrc.caching.scheduled.cache.CacheStatusCommand')
+def testAreasOfChangeNotQueriedIfCurrentlyCaching(cache_mock, query_mock):
+    cache_instance = cache_mock.return_value
+    cache_instance.execute.return_value = True
     
-def testAreasOfChangeQueriedIfNotCaching():  
-    with patch('agrc.caching.scheduled.cache.CacheStatusCommand') as cache_mock:
-        with patch('agrc.caching.scheduled.sde.AreasOfChangeQuery') as query_mock:
-            cache_instance = cache_mock.return_value
-            cache_instance.execute.return_value = False
-            
-            query_instance = query_mock.return_value
-            query_instance.execute.return_value = [AreaOfChange() for _ in xrange(2)]
-            
-            s = Runner()
-            s.start()
+    s = Runner()
+    s.start()
         
-    cache_mock.assert_called_once_with()
-    query_mock.assert_called_once_with()
+    print cache_mock.call_count
+    print query_mock.call_count
+ 
+    print dir(cache_instance)
+    
+    assert cache_mock.called
+    assert not query_mock.called
+
+@patch('agrc.caching.scheduled.sde.AreasOfChangeQuery')
+@patch('agrc.caching.scheduled.cache.CacheStatusCommand')  
+def testAreasOfChangeQueriedIfNotCaching(cache_mock, query_mock):
+    cache_instance = cache_mock.return_value
+    cache_instance.execute.return_value = False
+    
+    query_instance = query_mock.return_value
+    query_instance.execute.return_value = [AreaOfChange() for _ in xrange(2)]
+    
+    s = Runner()
+    s.start()
+        
+    assert cache_mock.called
+    assert query_mock.called
