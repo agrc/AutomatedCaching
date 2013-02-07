ba914cb723540be245331b94988166b636891fed
steve <sgourley@utah.gov>
2013-02-07 13:52:31 -0700

    mock out stubs so method doesn't continue

--- agrc/tests/testScheduler.py
+++ agrc/tests/testScheduler.py
@@ -17,16 +17,20 @@ class TestScheduleRunner(TestCase):
         assert cache_mock.called
         assert not query_mock.called
     
+    @patch('agrc.caching.scheduled.dto.GetCacheJobFromAreaOfChangeCommand')
     @patch('agrc.caching.scheduled.sde.AreasOfChangeQuery')
     @patch('agrc.caching.scheduled.cache.CacheStatusCommand')  
-    def test_areas_of_change_queried_if_not_caching(self, cache_mock, query_mock):
+    def test_areas_of_change_queried_if_not_caching(self, cache_mock, query_mock, job_mock):
         cache_instance = cache_mock.return_value
         cache_instance.execute.return_value = False
         
         query_instance = query_mock.return_value
-        query_instance.execute.return_value = [AreaOfChange() for _ in xrange(2)]
+        query_instance.execute.return_value = [AreaOfChange([0,1,2], "roads")]
         
-        s = Runner()
+        job_instance = job_mock.return_value
+        job_instance.execute.return_value = []
+        
+        s = Runner()        
         s.start()
             
         assert cache_mock.called
